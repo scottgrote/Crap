54f19ec9b4eb813ef29e2f5114487d0bcaa92775
scottgrote <scott.grote@smoothstack.com>
2020-07-15 20:15:39 -0400

    fuck cats man

--- src/src/App.js
+++ src/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and Dev Ops and Dynaimcs
+          Learn React and Dev Ops and Dynaimcs I fucking hate everything 
         </a>
       </header>
     </div>
